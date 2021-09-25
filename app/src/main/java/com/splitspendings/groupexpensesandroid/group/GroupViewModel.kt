d82b667730f778a579c0fa78f1b02234f2bf84cf
Oleksandr Bieliakov <abel9k@gmail.com>
2021-09-25 12:41:11 +0200

    added example of LiveData data binding with data transformation (counter text in fragment_group.xml)

--- app/src/main/java/com/splitspendings/groupexpensesandroid/group/GroupViewModel.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/group/GroupViewModel.kt
@@ -1,5 +1,21 @@
 package com.splitspendings.groupexpensesandroid.group
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
-class GroupViewModel(var groupName: String) : ViewModel()
+class GroupViewModel(var groupName: String) : ViewModel() {
+
+    private val _counter = MutableLiveData<Int>()
+    val counterString = Transformations.map(_counter) { counter ->
+        "Counter: $counter"
+    }
+
+    init {
+        _counter.value = 0
+    }
+
+    fun onIncreaseCounter() {
+        _counter.value = _counter.value?.inc()
+    }
+}
