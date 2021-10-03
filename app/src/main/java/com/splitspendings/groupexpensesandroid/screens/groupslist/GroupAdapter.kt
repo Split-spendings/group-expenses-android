8ca03796ab2d0029e4118e4ce47d8d4f3c12849f
Oleksandr Bieliakov <abel9k@gmail.com>
2021-10-03 14:22:56 +0200

    added new group list item layout and adjusted adapter and view holder for using it

--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/groupslist/GroupAdapter.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/groupslist/GroupAdapter.kt
@@ -1,16 +1,16 @@
 package com.splitspendings.groupexpensesandroid.screens.groupslist
 
-import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.splitspendings.groupexpensesandroid.R
+import com.splitspendings.groupexpensesandroid.common.formatGroup
 import com.splitspendings.groupexpensesandroid.repository.entities.Group
 
-class GroupItemViewHolder(val groupNameTextView: TextView) : RecyclerView.ViewHolder(groupNameTextView)
-
-class GroupAdapter : RecyclerView.Adapter<GroupItemViewHolder>() {
+class GroupAdapter : RecyclerView.Adapter<GroupAdapter.ViewHolder>() {
 
     var groups = listOf<Group>()
         set(value) {
@@ -18,21 +18,28 @@ class GroupAdapter : RecyclerView.Adapter<GroupItemViewHolder>() {
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.group_item_view, parent, false) as TextView
-        return GroupItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_group, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: GroupItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val group = groups[position]
-        holder.groupNameTextView.text = group.name
-        if(group.name.length < 2) {
-            holder.groupNameTextView.setTextColor(Color.RED)
-        } else {
-            holder.groupNameTextView.setTextColor(Color.BLACK)
-        }
+        val resources = holder.itemView.context.resources
+        holder.groupName.text = formatGroup(group, resources)
+        holder.avatar.setImageResource(
+            when (group.id?.mod(2)) {
+                0 -> R.drawable.ic_placeholder_group_avatar
+                else -> R.drawable.ic_placeholder_group_avatar_2
+            }
+        )
     }
 
     override fun getItemCount(): Int = groups.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val groupName: TextView = itemView.findViewById(R.id.group_name)
+        val avatar: ImageView = itemView.findViewById(R.id.group_avatar)
+    }
 }
