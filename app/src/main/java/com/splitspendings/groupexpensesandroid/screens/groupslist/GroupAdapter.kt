e740302fa5f7a98d34281a3e3f1937328e2c198c
Oleksandr Bieliakov <abel9k@gmail.com>
2021-10-03 17:04:58 +0200

    added data binding for group list item;

--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/groupslist/GroupAdapter.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/groupslist/GroupAdapter.kt
@@ -1,15 +1,13 @@
 package com.splitspendings.groupexpensesandroid.screens.groupslist
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.splitspendings.groupexpensesandroid.R
 import com.splitspendings.groupexpensesandroid.common.formatGroup
+import com.splitspendings.groupexpensesandroid.databinding.ListItemGroupBinding
 import com.splitspendings.groupexpensesandroid.repository.entities.Group
 
 class GroupAdapter : ListAdapter<Group, GroupAdapter.ViewHolder>(GroupDiffCallback()) {
@@ -23,15 +21,12 @@ class GroupAdapter : ListAdapter<Group, GroupAdapter.ViewHolder>(GroupDiffCallba
     }
 
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        private val groupName: TextView = itemView.findViewById(R.id.group_name)
-        private val avatar: ImageView = itemView.findViewById(R.id.group_avatar)
+    class ViewHolder private constructor(val binding: ListItemGroupBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(group: Group) {
             val resources = itemView.context.resources
-            groupName.text = formatGroup(group, resources)
-            avatar.setImageResource(
+            binding.groupName.text = formatGroup(group, resources)
+            binding.groupAvatar.setImageResource(
                 when (group.id?.mod(2)) {
                     0 -> R.drawable.ic_placeholder_group_avatar
                     else -> R.drawable.ic_placeholder_group_avatar_2
@@ -42,8 +37,8 @@ class GroupAdapter : ListAdapter<Group, GroupAdapter.ViewHolder>(GroupDiffCallba
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.list_item_group, parent, false)
-                return ViewHolder(view)
+                val binding = ListItemGroupBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
     }
