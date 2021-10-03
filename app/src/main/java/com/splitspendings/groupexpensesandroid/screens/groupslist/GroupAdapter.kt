a27da4c64ba7d8997cb2aedb159b37b1c2973188
Oleksandr Bieliakov <abel9k@gmail.com>
2021-10-03 14:58:15 +0200

    refactoring to remove view holder logic from adapter methods

--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/groupslist/GroupAdapter.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/groupslist/GroupAdapter.kt
@@ -19,27 +19,37 @@ class GroupAdapter : RecyclerView.Adapter<GroupAdapter.ViewHolder>() {
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.list_item_group, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val group = groups[position]
-        val resources = holder.itemView.context.resources
-        holder.groupName.text = formatGroup(group, resources)
-        holder.avatar.setImageResource(
-            when (group.id?.mod(2)) {
-                0 -> R.drawable.ic_placeholder_group_avatar
-                else -> R.drawable.ic_placeholder_group_avatar_2
-            }
-        )
+        holder.bind(group)
     }
 
     override fun getItemCount(): Int = groups.size
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val groupName: TextView = itemView.findViewById(R.id.group_name)
-        val avatar: ImageView = itemView.findViewById(R.id.group_avatar)
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val groupName: TextView = itemView.findViewById(R.id.group_name)
+        private val avatar: ImageView = itemView.findViewById(R.id.group_avatar)
+
+        fun bind(group: Group) {
+            val resources = itemView.context.resources
+            groupName.text = formatGroup(group, resources)
+            avatar.setImageResource(
+                when (group.id?.mod(2)) {
+                    0 -> R.drawable.ic_placeholder_group_avatar
+                    else -> R.drawable.ic_placeholder_group_avatar_2
+                }
+            )
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.list_item_group, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 }
