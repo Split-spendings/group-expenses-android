634517b318f7b9c159ee969d02dc3f6451da02d6
Oleksandr Bieliakov <abel9k@gmail.com>
2021-11-14 15:54:39 +0100

    added auth handler and auth state manager classes; added auth http connection builder

---
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/common/auth/AuthStateManager.kt
@@ -0,0 +1,51 @@
+package com.splitspendings.groupexpensesandroid.common.auth
+
+import net.openid.appauth.AuthState
+import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.TokenResponse
+
+/*
+ * Wraps the AuthState class from the AppAuth library
+ * Some or all of the auth state can be persisted to a secure location such as Encrypted Shared Preferences
+ */
+class AuthStateManager {
+
+    private var authState: AuthState? = null
+    var idToken: String? = null
+
+    /*
+     * Manage storing or updating the token response
+     */
+    fun saveTokens(tokenResponse: TokenResponse) {
+        // When refreshing tokens, the Identity Server does not issue a new ID token
+        // The AppAuth code does not allow us to update the token response with the original ID token
+        // Therefore we store the ID token separately
+        if (tokenResponse.idToken != null) {
+            this.idToken = tokenResponse.idToken
+        }
+
+        this.authState!!.update(tokenResponse, null)
+    }
+
+    /*
+    * Clear tokens upon logout or when the session expires
+    */
+    fun clearTokens() {
+        val metadata = this.authState?.authorizationServiceConfiguration
+        this.authState = AuthState(metadata!!)
+        this.idToken = null
+    }
+
+    var metadata: AuthorizationServiceConfiguration?
+        get() {
+            return this.authState?.authorizationServiceConfiguration
+        }
+        set(configuration) {
+            this.authState = AuthState(configuration!!)
+        }
+
+    val tokenResponse: TokenResponse?
+        get() {
+            return this.authState?.lastTokenResponse
+        }
+}
---
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/common/auth/HttpConnectionBuilder.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 The AppAuth for Android Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splitspendings.groupexpensesandroid.common.auth;
+
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import net.openid.appauth.Preconditions;
+import net.openid.appauth.connectivity.ConnectionBuilder;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Creates {@link HttpURLConnection} which allows HTTP connection.
+ */
+public final class HttpConnectionBuilder implements ConnectionBuilder {
+
+    /**
+     * The singleton instance of the connection builder.
+     */
+    public static final HttpConnectionBuilder INSTANCE = new HttpConnectionBuilder();
+
+    private static final int CONNECTION_TIMEOUT_MS = (int) TimeUnit.SECONDS.toMillis(15);
+    private static final int READ_TIMEOUT_MS = (int) TimeUnit.SECONDS.toMillis(10);
+
+    private HttpConnectionBuilder() {
+        // no need to construct instances of this type
+    }
+
+    @NonNull
+    @Override
+    public HttpURLConnection openConnection(@NonNull Uri uri) throws IOException {
+        Preconditions.checkNotNull(uri, "URI must not be null");
+        HttpURLConnection conn = (HttpURLConnection) new URL(uri.toString()).openConnection();
+        conn.setConnectTimeout(CONNECTION_TIMEOUT_MS);
+        conn.setReadTimeout(READ_TIMEOUT_MS);
+        conn.setInstanceFollowRedirects(false);
+        return conn;
+    }
+}
