8604c46a1dd208379622bee2f21634c94729f75a
Oleksandr Bieliakov <abel9k@gmail.com>
2021-11-14 15:31:34 +0100

    joined auth config and its loader in single file

--- app/src/main/java/com/splitspendings/groupexpensesandroid/common/auth/AuthConfig.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/common/auth/AuthConfig.kt
@@ -1,6 +1,14 @@
 package com.splitspendings.groupexpensesandroid.common.auth
 
+import android.content.Context
 import android.net.Uri
+import com.splitspendings.groupexpensesandroid.R
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import okio.Buffer
+import okio.buffer
+import okio.source
+import java.nio.charset.Charset
 
 /*
  * Standard mobile OAuth configuration settings
@@ -18,4 +26,26 @@ class AuthConfig {
     fun getRedirectUri(): Uri = Uri.parse(redirectUri)
 
     fun getPostLogoutRedirectUri(): Uri = Uri.parse(postLogoutRedirectUri)
+}
+
+class AuthConfigLoader {
+
+    /*
+     * Load configuration from the resource
+     */
+    fun load(context: Context): AuthConfig? {
+        // Get the raw resource
+        val stream = context.resources.openRawResource(R.raw.config)
+        val configSource = stream.source().buffer()
+
+        // Read it as JSON text
+        val configBuffer = Buffer()
+        configSource.readAll(configBuffer)
+        val configJson = configBuffer.readString(Charset.forName("UTF-8"))
+
+        // Deserialize it into objects
+        val moshi: Moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<AuthConfig> = moshi.adapter(AuthConfig::class.java)
+        return adapter.fromJson(configJson)
+    }
 }
