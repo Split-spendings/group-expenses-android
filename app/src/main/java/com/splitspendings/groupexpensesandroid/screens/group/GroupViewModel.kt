c0c1a31bab0b23426e1c94393e1ee4fdec4693c2
Oleksandr Bieliakov <abel9k@gmail.com>
2021-12-31 00:15:52 +0100

    removed spending logic from group view model

--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/group/GroupViewModel.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/group/GroupViewModel.kt
@@ -2,14 +2,9 @@ package com.splitspendings.groupexpensesandroid.screens.group
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.splitspendings.groupexpensesandroid.repository.GroupRepository
-import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class GroupViewModelFactory(
     private val groupId: Long,
@@ -33,34 +28,4 @@ class GroupViewModel(
     private val groupsRepository = GroupRepository.getInstance()
 
     val group = groupsRepository.getGroup(groupId)
-
-    val spendings = groupsRepository.getGroupSpendings(groupId)
-
-    private val _eventNavigateToSpending = MutableLiveData<Long>()
-    val eventNavigateToSpending: LiveData<Long>
-        get() = _eventNavigateToSpending
-
-    init {
-        _eventNavigateToSpending.value = null
-        loadGroupSpendings()
-    }
-
-    fun onSpendingClicked(id: Long) {
-        _eventNavigateToSpending.value = id
-    }
-
-    fun onEventNavigateToSpendingComplete() {
-        _eventNavigateToSpending.value = null
-    }
-
-    private fun loadGroupSpendings() {
-        viewModelScope.launch {
-            try {
-                //TODO: add endpoint that accepts filtering (e.g. by currency)
-                groupsRepository.refreshGroupSpendings(groupId)
-            } catch (e: Exception) {
-                Timber.d("Failure: ${e.message}")
-            }
-        }
-    }
 }
