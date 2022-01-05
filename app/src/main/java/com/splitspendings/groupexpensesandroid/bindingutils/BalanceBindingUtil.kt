babaa0690aca489d2e7ebc1c9aac5c2c91c94bd3
Oleksandr Bieliakov <abel9k@gmail.com>
2022-01-05 22:52:56 +0100

    added binding adapters for balance item balance amount color and payoff button visibility depending on positive/negative balance

--- app/src/main/java/com/splitspendings/groupexpensesandroid/bindingutils/BalanceBindingUtil.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/bindingutils/BalanceBindingUtil.kt
@@ -1,12 +1,40 @@
 package com.splitspendings.groupexpensesandroid.bindingutils
 
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.splitspendings.groupexpensesandroid.R
 import com.splitspendings.groupexpensesandroid.model.Balance
 import com.splitspendings.groupexpensesandroid.screens.balanceslist.BalancesListAdapter
+import java.math.BigDecimal
 
 @BindingAdapter("balancesList")
 fun bindBalancesListRecyclerView(recyclerView: RecyclerView, balancesList: List<Balance>?) {
     val adapter = recyclerView.adapter as BalancesListAdapter
     adapter.submitList(balancesList)
+}
+
+@BindingAdapter("balancePayoffButton")
+fun bindBalancePayoffButton(button: View, balance: Balance) {
+    if (balance.balance < BigDecimal.ZERO) {
+        button.visibility = VISIBLE
+    } else {
+        button.visibility = GONE
+    }
+}
+
+@BindingAdapter("balanceAmount")
+fun bindBalanceAmount(balanceAmount: TextView, balance: Balance) {
+    when {
+        balance.balance < BigDecimal.ZERO ->
+            balanceAmount.setTextColor(ContextCompat.getColor(balanceAmount.context, R.color.red))
+        balance.balance > BigDecimal.ZERO ->
+            balanceAmount.setTextColor(ContextCompat.getColor(balanceAmount.context, R.color.teal_700))
+        else ->
+            balanceAmount.setTextColor(ContextCompat.getColor(balanceAmount.context, R.color.grey))
+    }
 }
