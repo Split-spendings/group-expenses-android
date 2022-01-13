d0f209d9121e066e24d89871184dc5ca77e459cb
Oleksandr Bieliakov <abel9k@gmail.com>
2022-01-13 16:57:02 +0100

    updated group avatar icon style setting; commented out EXAMPLES of STRING RESOURCE HTML FORMATTING and BINDING ADAPTER AS EXTENSION FUNCTION TO VIEW

--- app/src/main/java/com/splitspendings/groupexpensesandroid/bindingutils/GroupBindingUtil.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/bindingutils/GroupBindingUtil.kt
@@ -1,29 +1,23 @@
 package com.splitspendings.groupexpensesandroid.bindingutils
 
-import android.content.res.Resources
-import android.os.Build
-import android.text.Html
-import android.text.Spanned
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.splitspendings.groupexpensesandroid.R
 import com.splitspendings.groupexpensesandroid.common.GroupsFilter
 import com.splitspendings.groupexpensesandroid.model.Group
 import com.splitspendings.groupexpensesandroid.screens.groupslist.GroupsListAdapter
 
-fun formatGroup(group: Group, resources: Resources): Spanned {
+//EXAMPLE of STRING RESOURCE HTML FORMATTING
+/*fun formatGroup(group: Group, resources: Resources): Spanned {
     val groupString = resources.getString(R.string.group_format, group.id, group.name)
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         Html.fromHtml(groupString, Html.FROM_HTML_MODE_LEGACY)
     } else {
         HtmlCompat.fromHtml(groupString, HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
-}
+}*/
 
-@BindingAdapter("groupNameFormattedPlaceholder")
+//EXAMPLE of implementing BINDING ADAPTER AS EXTENSION FUNCTION TO VIEW
+/*@BindingAdapter("groupNameFormattedPlaceholder")
 fun TextView.setGroupNameFormattedPlaceholder(group: Group?) {
     text = group?.let { formatGroup(it, context.resources) }
 }
@@ -36,7 +30,7 @@ fun ImageView.setGroupAvatarPlaceholder(group: Group?) {
             else -> R.drawable.ic_placeholder_group_avatar_2
         }
     )
-}
+}*/
 
 @BindingAdapter("groupsList", "filter")
 fun bindGroupsListRecyclerView(recyclerView: RecyclerView, groupsList: List<Group>?, filter: GroupsFilter) {
