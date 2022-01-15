7441d06ed48dc3d48a291a69f0d664d9ec12e4ae
Oleksandr Bieliakov <abel9k@gmail.com>
2022-01-15 22:27:39 +0100

    removed no longer needed spendings list fragment

--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/group/GroupPagerAdapter.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/group/GroupPagerAdapter.kt
@@ -1,12 +1,8 @@
 package com.splitspendings.groupexpensesandroid.screens.group
 
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.splitspendings.groupexpensesandroid.screens.balanceslist.BalancesListFragment
-import com.splitspendings.groupexpensesandroid.screens.spendingslist.SpendingsListFragment
-
 //EXAMPLE of TabLayout with ViewPager
 // (will not work properly with navigation and passing arguments to tabs fragments)
+/*
 class GroupPagerAdapter(val groupId: Long, private val groupFragment: GroupFragment) : FragmentStateAdapter(groupFragment) {
 
     override fun getItemCount(): Int = 2
@@ -15,4 +11,4 @@ class GroupPagerAdapter(val groupId: Long, private val groupFragment: GroupFragm
         0 -> SpendingsListFragment(groupId, groupFragment)
         else -> BalancesListFragment(groupId, groupFragment)
     }
-}
+}*/
