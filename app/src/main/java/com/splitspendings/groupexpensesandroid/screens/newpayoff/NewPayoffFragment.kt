12dfd6e68c1a3b8791386e4d140d85a5bba230b0
Oleksandr Bieliakov <abel9k@gmail.com>
2022-01-17 19:13:22 +0100

    updated new payoff fragment

--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/newpayoff/NewPayoffFragment.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/newpayoff/NewPayoffFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.splitspendings.groupexpensesandroid.R
+import com.splitspendings.groupexpensesandroid.common.Currency
 import com.splitspendings.groupexpensesandroid.common.closeKeyboard
 import com.splitspendings.groupexpensesandroid.databinding.FragmentNewPayoffBinding
+import com.splitspendings.groupexpensesandroid.model.GroupMember
+import timber.log.Timber
+import java.math.BigDecimal
+import java.util.*
 
 class NewPayoffFragment : Fragment() {
 
@@ -23,12 +30,12 @@ class NewPayoffFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_new_payoff, container, false)
 
         val args = NewPayoffFragmentArgs.fromBundle(requireArguments())
-        val balanceId = args.balanceId
         val groupId = args.groupId
+        val balanceId = args.balanceId
 
         val application = requireNotNull(activity).application
 
-        val viewModelFactory = NewPayoffViewModelFactory(balanceId, application)
+        val viewModelFactory = NewPayoffViewModelFactory(groupId, balanceId, application)
         viewModel = ViewModelProvider(this, viewModelFactory)[NewPayoffViewModel::class.java]
 
         binding.newPayoffViewModel = viewModel
@@ -37,9 +44,19 @@ class NewPayoffFragment : Fragment() {
         binding.editPayoffTitle.doAfterTextChanged { viewModel.title.value = it.toString() }
 
         viewModel.eventNavigateToPayoff.observe(viewLifecycleOwner, ::onNavigateToPayoff)
+        viewModel.groupMembersPaidFor.observe(viewLifecycleOwner, ::setUpPaidFor)
+        viewModel.groupMembersPaidTo.observe(viewLifecycleOwner, ::setUpPaidTo)
         viewModel.status.observe(viewLifecycleOwner, { it?.let { binding.statusLayout.status = it } })
 
+        setUpTotalAmount()
+        setUpCurrencyPicker()
+
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.onLoadGroupMembers()
     }
 
     override fun onDestroy() {
@@ -52,6 +69,103 @@ class NewPayoffFragment : Fragment() {
             findNavController()
                 .navigate(NewPayoffFragmentDirections.actionNewPayoffFragmentToPayoffFragment(payoffId))
             viewModel.onEventNavigateToPayoffComplete()
+        }
+    }
+
+    private fun setUpTotalAmount() {
+        binding.totalAmount.apply {
+            doAfterTextChanged { viewModel.onTotalAmountChanged(getNumericValueBigDecimal()) }
+            setLocale(Locale.getDefault())
+            setText(BigDecimal.ZERO.toString())
+        }
+    }
+
+    private fun setUpCurrencyPicker() {
+        binding.currencyPicker.apply {
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                Currency.values()
+            ).also { newAdapter ->
+                newAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                adapter = newAdapter
+            }
+            onItemSelectedListener = NewPayoffFragment.CurrencyPicker(viewModel)
+        }
+    }
+
+    private fun setUpPaidFor(groupMembers: List<GroupMember>?) {
+        binding.paidForPicker.apply {
+            if (groupMembers == null || groupMembers.isEmpty()) {
+                visibility = View.INVISIBLE
+                return
+            }
+
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                groupMembers.map { it.appUser.loginName }
+            ).also { newAdapter ->
+                newAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                adapter = newAdapter
+            }
+            onItemSelectedListener = NewPayoffFragment.PaidForPicker(viewModel, groupMembers)
+            visibility = View.VISIBLE
+        }
+    }
+
+    private fun setUpPaidTo(groupMembers: List<GroupMember>?) {
+        binding.paidToPicker.apply {
+            if (groupMembers == null || groupMembers.isEmpty()) {
+                visibility = View.INVISIBLE
+                return
+            }
+
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                groupMembers.map { it.appUser.loginName }
+            ).also { newAdapter ->
+                newAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                adapter = newAdapter
+            }
+            onItemSelectedListener = NewPayoffFragment.PaidToPicker(viewModel, groupMembers)
+            visibility = View.VISIBLE
+        }
+    }
+
+    class CurrencyPicker(val viewModel: NewPayoffViewModel) : AdapterView.OnItemSelectedListener {
+
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            viewModel.currency.value = Currency.values()[position]
+            Timber.d("currency selected: ${Currency.values()[position]}")
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+
+    class PaidForPicker(val viewModel: NewPayoffViewModel, private val groupMembers: List<GroupMember>) :
+        AdapterView.OnItemSelectedListener {
+
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            viewModel.paidFor.value = groupMembers[position]
+            Timber.d("paid for selected: ${groupMembers[position]}")
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+
+    class PaidToPicker(val viewModel: NewPayoffViewModel, private val groupMembers: List<GroupMember>) :
+        AdapterView.OnItemSelectedListener {
+
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            viewModel.paidTo.value = groupMembers[position]
+            Timber.d("paid to selected: ${groupMembers[position]}")
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
         }
     }
 }
--- app/src/main/java/com/splitspendings/groupexpensesandroid/screens/newpayoff/NewPayoffViewModel.kt
+++ app/src/main/java/com/splitspendings/groupexpensesandroid/screens/newpayoff/NewPayoffViewModel.kt
@@ -11,14 +11,20 @@ import androidx.lifecycle.viewModelScope
 import com.splitspendings.groupexpensesandroid.R
 import com.splitspendings.groupexpensesandroid.common.ApiStatus
 import com.splitspendings.groupexpensesandroid.common.Currency
+import com.splitspendings.groupexpensesandroid.common.SUCCESS_STATUS_MILLISECONDS
+import com.splitspendings.groupexpensesandroid.model.GroupMember
 import com.splitspendings.groupexpensesandroid.model.Status
 import com.splitspendings.groupexpensesandroid.network.dto.NewPayoffDto
 import com.splitspendings.groupexpensesandroid.repository.BalanceRepository
+import com.splitspendings.groupexpensesandroid.repository.GroupRepository
 import com.splitspendings.groupexpensesandroid.repository.PayoffRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.math.BigDecimal
 
 class NewPayoffViewModelFactory(
+    private val groupId: Long,
     private val balanceId: Long,
     private val application: Application
 ) : ViewModelProvider.Factory {
@@ -27,20 +33,24 @@ class NewPayoffViewModelFactory(
         if (!modelClass.isAssignableFrom(NewPayoffViewModel::class.java)) {
             throw IllegalArgumentException("Unknown ViewModel class")
         }
-        return NewPayoffViewModel(balanceId, application) as T
+        return NewPayoffViewModel(groupId, balanceId, application) as T
     }
 }
 
 const val MAX_PAYOFF_TITLE_SIZE = 100
 
 class NewPayoffViewModel(
+    val groupId: Long,
     balanceId: Long,
     val app: Application
 ) : AndroidViewModel(app) {
 
+    private val groupRepository = GroupRepository.getInstance()
     private val balanceRepository = BalanceRepository.getInstance()
     private val payoffRepository = PayoffRepository.getInstance()
 
+    val groupMembersPaidFor = groupRepository.getGroupMembers(groupId)
+    val groupMembersPaidTo = groupRepository.getGroupMembers(groupId)
     val balance = balanceRepository.getBalance(balanceId)
 
     private val _eventNavigateToPayoff = MutableLiveData<Long>()
@@ -51,7 +61,15 @@ class NewPayoffViewModel(
     val status: LiveData<Status>
         get() = _status
 
+    private val _submitStatus = MutableLiveData<Status>()
+    val submitStatus: LiveData<Status>
+        get() = _submitStatus
+
     val title = MutableLiveData<String>()
+    val paidFor = MutableLiveData<GroupMember>()
+    val paidTo = MutableLiveData<GroupMember>()
+    val amount = MutableLiveData<BigDecimal>()
+    val currency = MutableLiveData<Currency>()
 
     val submitButtonEnabled = Transformations.map(title) {
         it?.let {
@@ -71,6 +89,11 @@ class NewPayoffViewModel(
     init {
         _eventNavigateToPayoff.value = null
         _status.value = Status(ApiStatus.DONE, null)
+        _submitStatus.value = Status(ApiStatus.DONE, null)
+    }
+
+    fun onTotalAmountChanged(newTotalAmount: BigDecimal) {
+        amount.value = newTotalAmount
     }
 
     fun onSubmit() {
@@ -79,6 +102,23 @@ class NewPayoffViewModel(
 
     fun onEventNavigateToPayoffComplete() {
         _eventNavigateToPayoff.value = null
+    }
+
+    fun onLoadGroupMembers() {
+        viewModelScope.launch {
+            _status.value = Status(ApiStatus.LOADING, null)
+            try {
+                groupRepository.refreshGroupMembers(groupId)
+
+                _status.value = Status(ApiStatus.SUCCESS, app.getString(R.string.successful_group_members_upload))
+                delay(SUCCESS_STATUS_MILLISECONDS)
+                _status.value = Status(ApiStatus.DONE, null)
+
+            } catch (e: Exception) {
+                Timber.d("Failure: ${e.message}")
+                _status.value = Status(ApiStatus.ERROR, app.getString(R.string.failed_group_members_upload))
+            }
+        }
     }
 
     private fun savePayoffAndNavigateToPayoff() {
